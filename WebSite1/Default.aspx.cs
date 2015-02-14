5935288ecee326da58462391860f34c2ce6779ba
kinjalsolanki <kinjalsolanki26@gmail.com>
2015-02-14 11:44:10 +0530

    web service demo
    sample web service and sample website to consume it

---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        localhost.Service mys = new localhost.Service();   // you need to create the object of the web service 
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int c = mys.Multiplication(a, b);
+        TextBox3.Text = c.ToString();
+
+    }
+}
